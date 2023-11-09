e7d9c423743965c2215fd37bfe6553170c197c1a
JanisV <ignatov.vit@gmail.com>
2023-11-09 15:35:44 +0300

    Open image from command line parameter

--- src/run.py
+++ src/run.py
@@ -3,10 +3,13 @@ from PySide6.QtCore import QLibraryInfo, QLocale, QTranslator
 from PySide6.QtGui import QImage
 from PySide6.QtWidgets import QApplication, QDialog
 from ImageEditor import ImageEditorDialog
+from Tools import TemporaryDir
 
 
 def main():
     app = QApplication(sys.argv)
+
+    TemporaryDir.init('ImageEditor')
 
     locale = QLocale()
 
@@ -19,12 +22,20 @@ def main():
     if translator.load(locale, 'qtbase', '_', path):
         app.installTranslator(translator)
 
-    image = QImage("C:/Users/ignatov_vi/Documents/OpenNumismat/Area_20230309.png")
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+        image = QImage(file_name)
+    else:
+        image = None
 
     dlg = ImageEditorDialog()
     dlg.setWindowTitle('ImageEditor')
-    dlg.setImage(image)
+    if image:
+        dlg.setImage(image)
     dlg.exec()
+
+    # Clear temporary files
+    TemporaryDir.remove()
 
 
 if __name__ == "__main__":
