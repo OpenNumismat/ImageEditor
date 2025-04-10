ced9bc3a340f139118aadaa3ae128d58d3e30bca
JanisV <opennumismat@gmail.com>
2025-04-10 10:10:13 +0300

    Fix building

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .src.ImageEditor import ImageEditorDialog
+from .src.ImageEditor import ImageEditorDialog, SettingsDialog
 from .src.ImageProxy import ImageProxy
 from .src.ImageScrollLabel import ImageScrollLabel
 from .src import __version__
